8d215a4c75e90e9dbc9f3148928d6a37bc5b7a98
OJoaquin <joaquinoli27@gmail.com>
2025-08-20 11:32:57 -0300

    prueba

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,2 +1,6 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el código
+print("hola mundo")
+a=5
+b=7
+print(a+b)
