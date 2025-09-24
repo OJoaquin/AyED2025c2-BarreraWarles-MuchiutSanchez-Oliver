0e1ede6bc6b0bf4288204d9a73c3a3e60eea2319
unknown <martims276@gmail.com>
2025-09-24 12:46:47 -0300

    Grafica ordenamiento realizada

---
+++ TrabajoPractico_1/Algoritmos_de_ordenamiento/modules/Grafica_Tiempos_Ordenamientos.py
@@ -0,0 +1,49 @@
+from matplotlib import pyplot as plt #pip install matplotlib
+from random import randint
+import time
+from modules.Ordenamiento_Burbuja import OrdenamientoBurbuja
+from modules.Ordenamiento_Rapido import OrdenamientoRapido
+from modules.Ordenamiento_Residuo import ordenar_por_residuo
+tamanio = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
+
+tiempo_burbuja = []
+tiempo_rapido = []
+tiempo_residuo = []
+
+for n in tamanio:
+    lista = []
+
+    for _ in range(n):
+        dato = randint(1, 100)
+        lista.append(dato)
+    
+    #Ordenamiento Burbuja
+    inicio = time.perf_counter()
+    OrdenamientoBurbuja(lista)
+    fin = time.perf_counter()
+    tiempo_burbuja.append(fin-inicio)
+
+    #Ordenamiento Rapido
+    inicio = time.perf_counter()
+    OrdenamientoRapido(lista)
+    fin = time.perf_counter()
+    tiempo_rapido.append(fin-inicio)
+    
+    #Ordenamiento Residuo
+    inicio = time.perf_counter()
+    ordenar_por_residuo(lista)
+    fin = time.perf_counter()
+    tiempo_residuo.append(fin-inicio)
+
+
+# Gráfico para inserción
+plt.figure(figsize=(10, 6))
+plt.plot(tamanio, tiempo_burbuja, marker='o', label="Tiempo burbuja - O(n^2)")
+plt.plot(tamanio, tiempo_rapido, marker='o', label="Tiempo Rapido - O(nlog(n))")
+plt.plot(tamanio, tiempo_residuo, marker='o', label="Tiempo Residuo - O(n)")
+plt.xlabel('Tamaño de la lista')
+plt.ylabel('Tiempo (segundos)')
+plt.title('Comparación de tiempos de eliminación')
+plt.legend()
+plt.grid()
+plt.show()
