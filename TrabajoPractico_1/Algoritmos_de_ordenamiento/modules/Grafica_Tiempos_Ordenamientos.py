3a44fe31787a8758827feeecb844e66c1a5ce6c1
unknown <martims276@gmail.com>
2025-10-08 10:44:33 -0300

    Agregada grafica de sorted python

--- TrabajoPractico_1/Algoritmos_de_ordenamiento/modules/Grafica_Tiempos_Ordenamientos.py
+++ TrabajoPractico_1/Algoritmos_de_ordenamiento/modules/Grafica_Tiempos_Ordenamientos.py
@@ -9,6 +9,7 @@ tamanio = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
 tiempo_burbuja = []
 tiempo_rapido = []
 tiempo_residuo = []
+tiempo_sorted = []
 
 for n in tamanio:
     lista = []
@@ -16,7 +17,11 @@ for n in tamanio:
     for _ in range(n):
         dato = randint(1, 100)
         lista.append(dato)
-    
+    #Ordenamiento sorted python
+    inicio = time.perf_counter()
+    sorted(lista)
+    fin = time.perf_counter()
+    tiempo_sorted.append(fin-inicio)
     #Ordenamiento Burbuja
     inicio = time.perf_counter()
     OrdenamientoBurbuja(lista)
@@ -41,6 +46,7 @@ plt.figure(figsize=(10, 6))
 plt.plot(tamanio, tiempo_burbuja, marker='o', label="Tiempo burbuja - O(n^2)")
 plt.plot(tamanio, tiempo_rapido, marker='o', label="Tiempo Rapido - O(nlog(n))")
 plt.plot(tamanio, tiempo_residuo, marker='o', label="Tiempo Residuo - O(n)")
+plt.plot(tamanio, tiempo_sorted, marker='o', label="Tiempo sorted - O(n)")
 plt.xlabel('Tamaño de la lista')
 plt.ylabel('Tiempo (segundos)')
 plt.title('Comparación de tiempos de eliminación')
